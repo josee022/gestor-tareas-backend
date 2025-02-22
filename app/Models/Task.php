194f4864eab3a07414002de302eae034ac96840a
josee022 <jose.mondelo@iesdonana.org>
2025-02-22 15:48:40 +0100

    relacion folder tasks

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -23,4 +23,9 @@ class Task extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function folder()
+    {
+        return $this->belongsTo(Folder::class);
+    }
 }
