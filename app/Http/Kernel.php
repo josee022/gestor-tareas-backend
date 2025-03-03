c643eb08af67f8a8cedf97be61e362f0970130bd
josee022 <jose.mondelo@iesdonana.org>
2025-03-03 21:17:04 +0100

    cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -9,7 +9,8 @@ class Kernel extends HttpKernel
     protected $middleware = [
         \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
-        \Illuminate\Http\Middleware\HandleCors::class, // Agregar CORS
+        \App\Http\Middleware\CorsMiddleware::class,
+        \Illuminate\Http\Middleware\HandleCors::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
