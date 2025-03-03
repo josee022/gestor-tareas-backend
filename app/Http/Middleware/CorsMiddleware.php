01dd6f924694053cb3f54c515d2db9ef8cbc8711
josee022 <jose.mondelo@iesdonana.org>
2025-03-03 21:44:06 +0100

    cors

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -15,7 +15,8 @@ class CorsMiddleware
     {
         $response = $next($request);
 
-        $response->header('Access-Control-Allow-Origin', 'https://gestor-tareas-dk2yrlvcq-josee022s-projects.vercel.app');
+        // Permitir solicitudes desde tu frontend en Vercel
+        $response->header('Access-Control-Allow-Origin', '*');
         $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         $response->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
         $response->header('Access-Control-Allow-Credentials', 'true');
