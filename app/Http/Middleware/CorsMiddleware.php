4222892dea28b4245c9bf27e6b646be13d706db6
josee022 <jose.mondelo@iesdonana.org>
2025-03-03 22:33:40 +0100

    corsJARTO

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -16,7 +16,7 @@ class CorsMiddleware
         $response = $next($request);
 
         // Configuración CORS
-        $response->header('Access-Control-Allow-Origin', 'https://gestor-tareas-beige.vercel.app');
+        $response->header('Access-Control-Allow-Origin', '*');
         $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         $response->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
         $response->header('Access-Control-Allow-Credentials', 'true');
@@ -24,7 +24,7 @@ class CorsMiddleware
         // Manejo de preflight (opcional)
         if ($request->isMethod('OPTIONS')) {
             return response()->json('OK', 200, [
-                'Access-Control-Allow-Origin' => 'https://gestor-tareas-beige.vercel.app',
+                'Access-Control-Allow-Origin' => '*',
                 'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE, OPTIONS',
                 'Access-Control-Allow-Headers' => 'Content-Type, Authorization, X-Requested-With',
             ]);
