a5230f9bb671f32a4b5bb032f8f2e5c75d166db6
josee022 <jose.mondelo@iesdonana.org>
2025-03-04 09:35:25 +0100

    no funciona

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -14,6 +14,7 @@ class AuthController extends Controller
     public function __construct()
     {
         $this->middleware(EnsureFrontendRequestsAreStateful::class);
+        
     }
 
     public function register(Request $request)
