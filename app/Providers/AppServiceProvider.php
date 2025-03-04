6989df51226b8fc71eb8d9655b07b30c08508ac4
josee022 <jose.mondelo@iesdonana.org>
2025-03-04 09:25:40 +0100

    despliegue por fin2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
         Schema::defaultStringLength(191);
 
         // Forzar los headers CORS
-        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Origin: https://gestor-tareas-beige.vercel.app');
         header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
         header('Access-Control-Allow-Headers: Content-Type, Authorization');
     }
