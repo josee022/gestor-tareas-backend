686c90a8c865b31be936a6b41464e6683fce96d2
josee022 <jose.mondelo@iesdonana.org>
2025-03-04 09:50:15 +0100

    .

--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['cors'])->group(function () {
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
+    Route::get('/sanctum/csrf-cookie', function () {
+        return response()->json(['message' => 'CSRF cookie set']);
+    });
 });
 
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
